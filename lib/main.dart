8f7908b1920535ba2d9f6425bd51ca37a85200b0
adib <adibhaidarzaky@gmail.com>
2026-06-21 19:05:42 +0700

    setup initial smile cell

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+// TODO: Membuat splashscreen, input no hp screen, input pin screen, input otp screen, input data diri screen
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:smile_cell/pages/home_screen.dart';
+
+final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
+
+void main() {
+  runApp(const SmileCell());
+}
+
+class SmileCell extends StatelessWidget {
+  const SmileCell({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorObservers: [routeObserver],
+      title: 'Smile Cell',
+      theme: ThemeData(
+        textTheme: GoogleFonts.getTextTheme(
+          "Google Sans Flex",
+          Theme.of(context).textTheme
+        ),
+      ),
+      initialRoute: "/",
+      routes: {
+        // "/": (context) => SplashScreen(),
+        "/home": (context) => HomeScreen()
+      }
+    );
+  }
+}
